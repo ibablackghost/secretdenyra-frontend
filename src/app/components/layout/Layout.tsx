df39537f2fbac69b654f906f8ee18df9319b36a0
ibablackghost <papaibrahimadiagne2001@gmail.com>
2026-05-05 09:39:17 +0000

    fix du scroll automatique

--- src/app/components/layout/Layout.tsx
+++ src/app/components/layout/Layout.tsx
@@ -1,10 +1,22 @@
-import { Outlet } from 'react-router';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router';
 import { Header } from './Header';
 import { Footer } from './Footer';
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  }, [pathname]);
+
+  return null;
+};
 
 export const Layout = () => {
   return (
     <div className="min-h-screen flex flex-col font-['Mulish',sans-serif] bg-[#fafafa] text-[#131313]">
+      <ScrollToTop />
       <Header />
       <main className="flex-1 w-full">
         <Outlet />
