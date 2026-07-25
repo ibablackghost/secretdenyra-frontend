c1ac6e3d252da995c8a40c4d19a283a0391fad2e
ibablackghost <papaibrahimadiagne2001@gmail.com>
2026-07-25 12:20:22 +0000

    update :fix vulnerabilities2

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { RouterProvider } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
 import { router } from './routes';
 import { useAuthStore } from './store/authStore';
 import { useWishlistStore } from './store/wishlistStore';
